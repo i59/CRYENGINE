47464d77d60bc6e93bbde4fa25074646548a4905
Filip Lundgren <filip@poppermost.se>
2016-07-13 20:22:18 +0200

    Added simple Mannequin setup with one idle anim

---
+++ TemplateFirstPersonShooter/Animations/Mannequin/ADB/FirstPerson.adb
@@ -0,0 +1,12 @@
+<AnimDB FragDef="Animations/Mannequin/ADB/PlayerFragmentIds.xml" TagDef="Animations/Mannequin/ADB/PlayerTags.xml">
+ <FragmentList>
+  <Idle>
+   <Fragment BlendOutDuration="0.2" Tags="">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.2"/>
+     <Animation name="stand_tac_idle_nw_3p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+  </Idle>
+ </FragmentList>
+</AnimDB>
