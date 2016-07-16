4132686913d12476de28ef1a65e7e6944e230b09
Filip Lundgren <filip@poppermost.se>
2016-07-16 12:26:50 +0200

    Move bullet entity scale to CVar

--- Code/GameTemplates/cpp/FirstPersonShooter/GameDll/Entities/Gameplay/Weapons/Rifle.cpp
+++ Code/GameTemplates/cpp/FirstPersonShooter/GameDll/Entities/Gameplay/Weapons/Rifle.cpp
@@ -16,10 +16,13 @@ class CRifleRegistrator
 	void RegisterCVars()
 	{
 		m_pGeometryPath = REGISTER_STRING("w_rifleGeometryPath", "Objects/Weapons/Rifle/rifle_tp.cgf", VF_CHEAT, "The name of the FP context used in Mannequin");
+
+		REGISTER_CVAR2("w_rifleBulletScale", &m_bulletScale, 0.05f, VF_CHEAT, "Determines the scale of the bullet geometry");
 	}
 
 public:
 	ICVar *m_pGeometryPath;
+	float m_bulletScale;
 };
 
 CRifleRegistrator g_rifleRegistrator;
@@ -38,7 +41,8 @@ void CRifle::RequestFire()
 
 	spawnParams.vPosition = GetEntity()->GetWorldPos() + GetEntity()->GetWorldRotation() * Vec3(0, 1, 1);
 	spawnParams.qRotation = GetEntity()->GetWorldRotation();
-	spawnParams.vScale = Vec3(0.05f);
+
+	spawnParams.vScale = Vec3(g_rifleRegistrator.m_bulletScale);
 
 	// Spawn the entity, bullet is propelled in CBullet based on the rotation and position here
 	gEnv->pEntitySystem->SpawnEntity(spawnParams);
