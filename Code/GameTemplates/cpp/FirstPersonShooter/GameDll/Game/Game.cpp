c8049e755cafcc7be04c4bd4065f5d2f7b9388e8
Filip Lundgren <filip@poppermost.se>
2016-07-17 15:12:35 +0200

    Log into player profile on game startup

--- Code/GameTemplates/cpp/FirstPersonShooter/GameDll/Game/Game.cpp
+++ Code/GameTemplates/cpp/FirstPersonShooter/GameDll/Game/Game.cpp
@@ -6,6 +6,8 @@
 #include "Game.h"
 #include "GameFactory.h"
 
+#include <IPlayerProfiles.h>
+
 CGame::CGame()
 	: m_pGameFramework(nullptr)
 {
@@ -28,7 +30,27 @@ bool CGame::Init(IGameFramework* pFramework)
 	CGameFactory::Init();
 	m_pGameFramework->SetGameGUID(GAME_GUID);
 
+	// We hafve to initialize the player profile system
+	// Otherwise level load will fail in action map initialization
+	InitializePlayerProfile();
+
 	return true;
+}
+
+void CGame::InitializePlayerProfile()
+{
+	auto &profileManager = *m_pGameFramework->GetIPlayerProfileManager();
+
+	profileManager.Initialize();
+
+	bool bFirstLogin = true;
+	if (!profileManager.LoginUser("Player", bFirstLogin))
+	{
+		CryWarning(VALIDATOR_MODULE_GAME, VALIDATOR_ERROR, "Failed to login player profile user.");
+		return;
+	}
+
+	profileManager.ActivateProfile("Player", "default");
 }
 
 void CGame::RegisterGameFlowNodes()
